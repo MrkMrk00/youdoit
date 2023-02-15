8c31b01ef7a3f9bd9fe05abc28950c5673a05e5e
Hong Anh  Tranová <tranhaova0123@gmail.com>
2023-02-15 17:09:50 +0100

    Admin: Recipe user

--- admin/components/Recipe.tsx
+++ admin/components/Recipe.tsx
@@ -1,11 +1,29 @@
-import { Component, DateTimeField, Repeater, RichTextField, TextField } from '@contember/admin'
+import { Component, DateTimeField, Field, Repeater, RichTextField, SelectField, TextField } from '@contember/admin'
 import { ImageField } from './ImageField'
 import { LocaleSideDimension } from './LocaleSideDimensions'
 import { StepGroup } from './StepGroup'
 
-export const Recipe = Component(() => {
+export const Recipe = Component((_, enviroment) => {
+	console.log(enviroment)
 	return (
 		<>
+			<SelectField
+				field="createdBy"
+				label="Created by"
+				options="User"
+				renderOption={(accessor) => {
+					const firstName = accessor.getField('firstName').value
+					const lastName = accessor.getField('lastName').value
+
+					return firstName + ' ' + lastName
+				}}
+				optionsStaticRender={
+					<>
+						<Field field="firstName" />
+						<Field field="lastName" />
+					</>
+				}
+			/>
 			<DateTimeField field="publishDate" label="Publish date" />
 			<TextField field="price" label="Price" />
 			<ImageField field="mainImage" label="Main image" />
