3007cdc1552e93b25cae3e236436f1a008ccbea9
Hong Anh Tranová <tranhaova0123@gmail.com>
2023-04-01 21:26:39 +0200

    TipGroup: admin

--- admin/components/Recipe.tsx
+++ admin/components/Recipe.tsx
@@ -7,6 +7,7 @@ import {
 	MultiSelectField,
 	Repeater,
 	RichTextField,
+	Section,
 	SelectField,
 	TextField,
 } from '@contember/admin'
@@ -14,63 +15,77 @@ import { ImageField } from './ImageField'
 import { LinkUrlField } from './LinkUrlField'
 import { LocaleSideDimension } from './LocaleSideDimensions'
 import { StepGroup } from './StepGroup'
+import { TipGroup } from './TipGroup'
 
 export const Recipe = Component((_, enviroment) => {
 	return (
 		<>
-			<LocaleSideDimension>
-				<LinkUrlField derivedFrom="title" />
-			</LocaleSideDimension>
-			<SelectField
-				field="createdBy"
-				label="Created by"
-				options="User"
-				renderOption={(accessor) => {
-					const firstName = accessor.getField('firstName').value
-					const lastName = accessor.getField('lastName').value
+			<Section heading="Basic informations">
+				<LocaleSideDimension>
+					<LinkUrlField derivedFrom="title" />
+				</LocaleSideDimension>
+				<SelectField
+					field="createdBy"
+					label="Created by"
+					options="User"
+					renderOption={(accessor) => {
+						const firstName = accessor.getField('firstName').value
+						const lastName = accessor.getField('lastName').value
 
-					return firstName + ' ' + lastName
-				}}
-				optionsStaticRender={
-					<>
-						<Field field="firstName" />
-						<Field field="lastName" />
-					</>
-				}
-			/>
-			<DateTimeField field="publishDate" label="Publish date" />
-			<MultiSelectField
-				field="categories"
-				label="Categories"
-				options="Category"
-				renderOption={(e) => (
-					<Entity accessor={e}>
+						return firstName + ' ' + lastName
+					}}
+					optionsStaticRender={
+						<>
+							<Field field="firstName" />
+							<Field field="lastName" />
+						</>
+					}
+				/>
+				<DateTimeField field="publishDate" label="Publish date" />
+				<LocaleSideDimension>
+					<TextField field="title" label="Title" />
+					<RichTextField field="description" label="Description" />
+				</LocaleSideDimension>
+				<TextField field="price" label="Price" />
+				<Box heading="Tile">
+					<ImageField field="tileImage" label="Tile image" />
+					<LocaleSideDimension>
+						<RichTextField field="tileDescription" label="Tile description" />
+					</LocaleSideDimension>
+				</Box>
+			</Section>
+			<Section heading="Categories">
+				<MultiSelectField
+					field="categories"
+					label="Categories"
+					options="Category"
+					renderOption={(e) => (
+						<Entity accessor={e}>
+							<LocaleSideDimension>
+								<Field field="title" />
+							</LocaleSideDimension>
+						</Entity>
+					)}
+					optionsStaticRender={
 						<LocaleSideDimension>
 							<Field field="title" />
 						</LocaleSideDimension>
-					</Entity>
-				)}
-				optionsStaticRender={
-					<LocaleSideDimension>
-						<Field field="title" />
-					</LocaleSideDimension>
-				}
-			/>
-			<TextField field="price" label="Price" />
-			<Box heading="Tile">
-				<ImageField field="tileImage" label="Tile image" />
-				<LocaleSideDimension>
-					<RichTextField field="tileDescription" label="Tile description" />
-				</LocaleSideDimension>
-			</Box>
-			<ImageField field="mainImage" label="Main image" />
-			<LocaleSideDimension>
-				<TextField field="title" label="Title" />
-				<RichTextField field="description" label="Description" />
-			</LocaleSideDimension>
-			<Repeater field="stepsGroups" label="Step groups" sortableBy="order">
-				<StepGroup />
-			</Repeater>
+					}
+				/>
+			</Section>
+			<Section heading="Images">
+				<ImageField field="mainImage" label="Main image" />
+			</Section>
+			<Section heading="Steps">
+				<Repeater field="stepsGroups" label="Step groups" sortableBy="order">
+					<StepGroup />
+				</Repeater>
+			</Section>
+			<Section heading="Tips">
+				<Repeater field="tipGroups" label="Tip groups" sortableBy="order">
+					<TipGroup />
+				</Repeater>
+			</Section>
 		</>
 	)
 })
