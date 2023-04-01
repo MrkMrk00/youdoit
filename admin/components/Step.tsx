eccf28fb9fae762febb4a457b240a945d8dcb3d8
Hong Anh Tranová <tranhaova0123@gmail.com>
2023-04-01 23:31:42 +0200

    Step timer: admin

--- admin/components/Step.tsx
+++ admin/components/Step.tsx
@@ -1,4 +1,4 @@
-import { Component, HasOne, TextField } from '@contember/admin'
+import { Box, CheckboxField, Component, HasOne, NumberField, TextField } from '@contember/admin'
 import { LocaleSideDimension } from './LocaleSideDimensions'
 
 interface StepProps {
@@ -11,6 +11,10 @@ export const Step = Component<StepProps>((props) => {
 			<LocaleSideDimension>
 				<TextField field="title" label="Title" />
 			</LocaleSideDimension>
+			<Box heading="Timer">
+				<CheckboxField field="hasTimer" label="Has timer" />
+				<NumberField field="timerCount" label="Timer count" labelDescription="Type time in seconds" />
+			</Box>
 		</HasOne>
 	)
 })
