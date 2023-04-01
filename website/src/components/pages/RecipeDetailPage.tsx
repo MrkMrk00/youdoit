cc122d603404820b33b60a1c57971bb2ded9fe58
Hong Anh Tranová <tranhaova0123@gmail.com>
2023-04-02 00:54:54 +0200

    WIP

--- website/src/components/pages/RecipeDetailPage.tsx
+++ website/src/components/pages/RecipeDetailPage.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx'
 import type { FunctionComponent } from 'react'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import type { RecipeLocaleResult } from '../../data/RecipeLocaleFragment'
 import { Container } from '../Container'
 import { DetailHeader } from '../DetailHeader'
@@ -20,38 +21,48 @@ export const RecipeDetailPage: FunctionComponent<RecipeDetailPageProps> = ({ rec
 		publishDate: recipeDetailPage.base?.publishDate,
 	}
 
-	return (
-		<div>
-			<DetailHeader
-				allRecipesLink={allRecipesLink}
-				image={recipeDetailPage.base?.mainImage}
-				title={recipeDetailPage.title}
-				description={recipeDetailPage.description}
-				author={author}
-			/>
-			<Container>
-				<div className={styles.content}>
-					<div className={styles.tipGroupList}>
-						{recipeDetailPage.base?.tipGroups.map((group) => {
-							return (
-								<Fragment key={group.id}>
-									<TipGroup group={group} />
-								</Fragment>
-							)
-						})}
-					</div>
+	const [activeStep, setActiveStep] = useState<number | null>(1)
 
-					<div className={styles.stepGroupList}>
-						{recipeDetailPage.base?.stepsGroups.map((group, index) => {
-							return (
-								<Fragment key={group.id}>
-									<StepGroup group={group} index={index + 1} />
-								</Fragment>
-							)
-						})}
-					</div>
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.overlay} />
+				<div className={styles.header}>
+					<DetailHeader
+						allRecipesLink={allRecipesLink}
+						image={recipeDetailPage.base?.mainImage}
+						title={recipeDetailPage.title}
+						description={recipeDetailPage.description}
+						author={author}
+					/>
 				</div>
-			</Container>
-		</div>
+				<div className={styles.contentIn}>
+					<Container>
+						<div className={styles.tipGroupList}>
+							{recipeDetailPage.base?.tipGroups.map((group) => {
+								return (
+									<Fragment key={group.id}>
+										<TipGroup group={group} />
+									</Fragment>
+								)
+							})}
+						</div>
+
+						<div className={styles.stepGroupList}>
+							{recipeDetailPage.base?.stepsGroups.map((group, index) => {
+								const isActive = activeStep === index
+								return (
+									<Fragment key={group.id}>
+										<div className={clsx(styles.stepGroup, isActive && styles.isActive)}>
+											<StepGroup group={group} index={index + 1} />
+										</div>
+									</Fragment>
+								)
+							})}
+						</div>
+					</Container>
+				</div>
+			</div>
+		</>
 	)
 }
