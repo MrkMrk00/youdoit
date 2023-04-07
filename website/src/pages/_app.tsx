6460a18a7c1bd0a24b9693f236a8d06b2b48500e
Hong Anh Tranová <tranhaova0123@gmail.com>
2023-04-07 13:57:43 +0200

    @tanstack/react-query installed

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { Inter } from '@next/font/google'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
 import { SharedLoadingIndicatorContextProvider, SharedProgressLoadingIndicator } from 'shared-loading-indicator'
 import { PageNavigationLoadingTracker } from '../components/PageNavigationLoadingTracker'
 import '../styles/globals.sass'
@@ -7,8 +9,19 @@ import '../styles/globals.sass'
 const inter = Inter()
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const queryClient = useMemo(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+					},
+				},
+			}),
+		[],
+	)
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<style jsx global>{`
 				html {
 					font-family: ${inter.style.fontFamily};
@@ -19,7 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<SharedProgressLoadingIndicator />
 				<Component {...pageProps} />
 			</SharedLoadingIndicatorContextProvider>
-		</>
+		</QueryClientProvider>
 	)
 }
 
