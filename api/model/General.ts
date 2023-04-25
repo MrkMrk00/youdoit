d64bac20c11947534b85d5f9c50fd1bda5238348
Hong Anh Tranová <tranhaova0123@gmail.com>
2023-04-25 17:29:50 +0200

    General unique

--- api/model/General.ts
+++ api/model/General.ts
@@ -1,9 +1,12 @@
 import { AclDefinition as acl, SchemaDefinition as d } from '@contember/schema-definition'
+import { One } from './One'
 import { Seo } from './Seo'
 import { publicRole } from './acl'
 
 @acl.allow(publicRole, { read: true })
 export class General {
+	unique = d.enumColumn(One).notNull().unique()
+
 	// Webmanifest
 	name = d.stringColumn()
 	shortName = d.stringColumn()
